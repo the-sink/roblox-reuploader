5cc6240b5068acdc5e879c3bb0306e76eb807bbb
the-sink <lucianedean@gmail.com>
2022-03-14 22:05:39 -0700

    Update config example notice

--- config.example.py
+++ config.example.py
@@ -1,6 +1,5 @@
 roblosecurity = "" # Insert .ROBLOSECURITY cookie here in the quotes
 rbxid = "" # Insert .RBXID cookie here in the quotes
 
-# Yes, normally it is a *very bad idea* to put these cookies anywhere, but they are required for the API endpoint that
-# provides a means of obtaining bulk asset information. The source code is available right here, in this folder,
-# if you're concerned (obviously)
+# It is normally a VERY BAD IDEA to share these cookies with anyone. But, in this case, they are required for the API endpoint that
+# provides a means of obtaining bulk asset information. The source code is available right here, in this folder, if you're concerned (obviously).
